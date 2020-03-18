2f0db14d00b9601e3d1d07a65de65269fa711b06
Luke <lukehenderson1996@yahoo.com>
2020-03-18 10:13:41 -0500

    Better Error Handling

--- bitfinex.py
+++ bitfinex.py
@@ -5,6 +5,6 @@ from time import sleep
 url = "https://api.bitfinex.com/v1/pubticker/btcusd"
 
 while True:
-    sleep(4) # 4 is the right delay
+    sleep(4) # 4 is the right delay, actually we're trying 6 now
     response = requests.request("GET", url)
     print(response.text)
