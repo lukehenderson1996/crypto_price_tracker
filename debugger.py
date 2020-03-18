3ce64505139a1a1d7c971597742cd93119ee7e2d
Luke <lukehenderson1996@yahoo.com>
2020-03-18 10:46:25 -0500

    File Organization Began

--- debugger.py
+++ debugger.py
@@ -1,10 +1,16 @@
 #updated for python 3 usage
 import requests, json
+import time
 from time import sleep, localtime, strftime
-
+import sys
+import os
 import logging
 
 
+if not os.path.exists('csv/dated' + strftime("%Y-%m-%d_%H", localtime())):
+    os.mkdir('csv/dated' + strftime("%Y-%m-%d_%H", localtime()))
 
+f=open('csv/dated/BTC.csv', "a+")
+f.write("Time,Kraken,Bitstamp,Bitfinex,Bitflyer,Itbit\r\n")
 
-print("Error querying Bitstamp API")
+print(strftime("%Y-%m-%d_%H", localtime()))
