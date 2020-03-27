4aab8c857adbb5ea17fd4d9529423b5ec27c6f5e
Luke <LukeHenderson817@yahoo.com>
2020-03-26 21:49:24 -0500

    Began file manager

---
+++ file_manager.py
@@ -0,0 +1,16 @@
+#updated for python 3 usage
+import requests, json
+import time
+from time import sleep, localtime, strftime
+import sys
+import os
+import logging
+
+
+lastRun = 0 #time.time() #time in seconds, when files were last managed
+
+while true:
+    if true: #if it's just past midnight and its been 2 hours since last run
+        #
+    else:
+        sleep(600) #sleep 10 min
