76aaf94c0157f4d1401ac79969bb7d11fa0a2b56
Luke <LukeHenderson817@yahoo.com>
2020-03-27 19:18:51 -0500

    file man updates

--- debugger.py
+++ debugger.py
@@ -5,15 +5,9 @@ from time import sleep, localtime, strftime
 import sys
 import os
 import logging
+from parse import *
 
-
-fetchTime = localtime()
-
-
-if not os.path.exists('datedCSV/' + strftime("%Y-%m-%d", fetchTime)):
-    os.mkdir('datedCSV/' + strftime("%Y-%m-%d", fetchTime))
-
-f=open('datedCSV/' + strftime("%Y-%m-%d", fetchTime) + '/BTC_' + strftime("%H", fetchTime) + '.csv', "a+")
-f.write("This is a test\r\n")
+#getting subfolders
+print(os.walk('.')[1])
 
 print("done")
--- file_manager.py
+++ file_manager.py
@@ -1,16 +1,54 @@
 #updated for python 3 usage
-import requests, json
 import time
 from time import sleep, localtime, strftime
 import sys
 import os
-import logging
+from parse import * #pip install parse
+#functionality bucket list:
+    #run this on a schedule
+    #automatically upload to google drive by reading password from a document on computer not on github, also use webcrawler gmail
+    #insert fillers into daily files in case of missing data
 
 
-lastRun = 0 #time.time() #time in seconds, when files were last managed
+def upLogFile(lastRun):
+    f=open('datedCSV/filemanlog', "w")
+    f.write("lastRun: " + str(lastRun) + ";\r\n")
+    f.close()
 
-while true:
-    if true: #if it's just past midnight and its been 2 hours since last run
-        #
-    else:
-        sleep(600) #sleep 10 min
+
+#init
+# lastRun, when files were last managed in seconds
+if not os.path.exists('datedCSV/filemanlog'): #file does not yet exist
+    lastRun = 0
+    upLogFile(lastRun)
+else:
+    #read log for variables
+    logFile=open("datedCSV/filemanlog", "r")
+    logFileCont =logFile.read()
+    lastRun = float(parse("lastRun: {};", logFileCont)[0])
+    logFile.close()
+
+
+#main loop
+while True:
+    #look for kill command
+    endProc=open("kill_process", "r")
+    endProcCont =endProc.read()
+    if endProcCont[:4] == "true":
+        exit()
+    endProc.close()
+
+    #update log file
+    lastRun = time.time()
+    upLogFile(lastRun)
+
+    #concatenate hourly files into daily files
+    #make list of days and generate appropriate for loop, running earliest first
+        #if current day is earlier than lastRun
+            #create file/header for that day and start filling with each hour (if it exists)
+
+
+
+
+    #exit
+    exit()
